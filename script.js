c7455c0c25c8ccb00d3624c6a6899b0998c0534e
Donald <donnnie@Donalds-iMac.local>
2018-03-28 23:53:51 +0100

    Removed selection highlight in iOS safari

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ window.onload = function () {
         });
 
     canvas.addEventListener('mousedown', handleMouseClick);
-    canvas.addEventListener('touchstart', handleMouseClick);
+    canvas.addEventListener('touchstart', handleMouseClick, true);
 
     canvas.addEventListener('touchmove', function (evt) {
         var touchobj = evt.touches[0];
